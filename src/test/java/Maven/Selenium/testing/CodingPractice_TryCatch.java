aaed359ca51504da2224b6c4bc9075c631a5523a
Rintu <rintu_8@rediffmail.com>
2025-06-18 11:02:28 +0530

    TrtCatch

---
+++ src/test/java/Maven/Selenium/testing/CodingPractice_TryCatch.java
@@ -0,0 +1,32 @@
+package Maven.Selenium.testing;
+
+public class CodingPractice_TryCatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Example 1
+		
+//		try {
+//			
+//			int myInt = Integer.parseInt("pants");
+//			
+//		}
+//		
+//		catch(NumberFormatException nfe)
+//		//We can also use catch(exception e) that catches all exception
+//		
+//		{
+//			System.out.println("Note that we cant get an integer from a string");
+//			
+//		}
+		
+		
+		//===========================================
+		
+		// Other examples of exceptions are Throwable , 
+		//Runtime exception , IllegalArguementException
+
+	}
+
+}
